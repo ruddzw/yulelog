68d648b3f4ce1891e272fa81a7e61193ec5e7b65
Rudd Zwolinski <ruddzw@gmail.com>
2014-12-15 19:24:48 -0500

    Poking the log moves it. Also name

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Log : YuleMonoBehaviour {
     public ParticleSystem sparkSystem;
 
+    public Vector3 pokeOffset;
+
     public void SparkAtPoint(Vector2 point2d, GameObject logPart) {
         sparkSystem.transform.position = new Vector3(point2d.x, point2d.y, sparkSystem.transform.position.z);
 
@@ -11,5 +13,9 @@ public class Log : YuleMonoBehaviour {
         sparkSystem.Clear();
         sparkSystem.Simulate(0.005f, true); // Workaround for weird behavior from http://forum.unity3d.com/threads/moving-a-particlesystem-why-does-unity-interpolate-its-position.134283/
         sparkSystem.Play();
+
+        LeanTween.move(gameObject, transform.position + pokeOffset, 0.1f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+            LeanTween.move(gameObject, transform.position - pokeOffset, 0.1f).setEase(LeanTweenType.easeInOutQuad);
+        });
     }
 }
